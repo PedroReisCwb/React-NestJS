969fb26f173275759f879c1e42e3b4db0d42ebbd
Pedro Reis <reis.cwb@hotmail.com>
2021-03-16 11:33:32 -0300

    Adicionando projeto
    Versionando projeto

---
+++ src/idea/idea.dto.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { IsString } from 'class-validator';
+
+export class IdeaDTO {
+  @IsString()
+  idea: string;
+
+  @IsString()
+  description: string;
+}
---
+++ src/idea/idea.entity.ts
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('idea')
+export class IdeaEntity {
+  @PrimaryGeneratedColumn() id: number;
+
+  @CreateDateColumn() created: Date;
+
+  @Column('text') idea: string;
+
+  @Column('text') description: string;
+}
